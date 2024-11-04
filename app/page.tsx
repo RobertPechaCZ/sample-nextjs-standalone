006b67e661ed38f0161cfe5ba5c5fd56dbf0602d
RobertPechaCZ <bofteam@centrum.cz>
2024-11-04 23:18:35 +0100

    fleek fleek fleek

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
                         <Image
                             className="scalingo-logo"
                             alt="Scalingo"
-                            src="https://scalingo.com/logo-lightgrey.svg"
+                            src="https://fleek.xyz/svg/fleek-logo.svg"
                             width={330}
                             height={80}
                             priority
@@ -18,7 +18,7 @@ export default function Home() {
                         <Image
                             className="heart-logo"
                             alt="love"
-                            src="https://cdn.scalingo.com/documentation/heart.png"
+                            src="https://fleek.xyz/images/landing-page/partners/filecoin.svg"
                             width={80}
                             height={80}
                             priority
@@ -36,14 +36,14 @@ export default function Home() {
                 </div>
                 <h1>Deployed on Fleek Network!</h1>
                 <h2 className="mt-16 mb-12">
-                    <span>Deploying your own Next.js app on Scalingo is as easy as pie. </span>
+                    <span>Deploying your own Next.js app on Fleek Network is as easy as pie. </span>
                     <div className="my-4">
                         <span>Click to deploy:</span>
                         <a className="btn"
                            href="https://my.osc-fr1.scalingo.com/deploy?source=https://github.com/Scalingo/sample-nextjs-standalone">
                             <Image
                                 src="/click-to-deploy.svg"
-                                alt="Scalingo click to deploy icon"
+                                alt="Fleek click to deploy icon"
                                 width={178}
                                 height={40}
                                 priority
@@ -53,8 +53,8 @@ export default function Home() {
                     <span className="block">It will be up in less than 2 minutes!</span>
                 </h2>
                 <p>The code of this sample lives <a
-                    href="https://github.com/Scalingo/sample-nextjs-standalone">on GitHub</a>. You
-                    can find <a href="https://samples.scalingo.com">more samples here</a>.</p>
+                    href="https://github.com/fleek-platform/sdk">on GitHub</a>. You
+                    can find <a href="https://docs.fleek.xyz">more samples here</a>.</p>
             </div>
         </main>
     );
